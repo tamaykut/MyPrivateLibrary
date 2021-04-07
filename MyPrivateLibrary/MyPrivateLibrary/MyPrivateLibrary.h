e91c61d9cba817f06859302d1321826f4c6a3950
Tamay Aykut <tamay.aykut@outlook.com>
2021-04-06 17:18:23 -0700

    initial commit

---
+++ MyPrivateLibrary/MyPrivateLibrary/MyPrivateLibrary.h
@@ -0,0 +1,18 @@
+//
+//  MyPrivateLibrary.h
+//  MyPrivateLibrary
+//
+//  Created by Tamay Aykut on 4/6/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyPrivateLibrary.
+FOUNDATION_EXPORT double MyPrivateLibraryVersionNumber;
+
+//! Project version string for MyPrivateLibrary.
+FOUNDATION_EXPORT const unsigned char MyPrivateLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyPrivateLibrary/PublicHeader.h>
+
+

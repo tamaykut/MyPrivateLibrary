f7e201be5a0af7a8cdb54e4a908ba2a4ab65b474
Tamay Aykut <tamay.aykut@outlook.com>
2021-04-06 21:34:07 -0700

    add TFLite

--- MyPrivateLibrary/MyPrivateLibrary/PrivatePrinter.swift
+++ MyPrivateLibrary/MyPrivateLibrary/PrivatePrinter.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import TensorFlowLite
 
 public struct PrivatePrinter {
     public init() {
---
+++ MyPrivateLibrary/Pods/Target Support Files/Pods-MyPrivateLibrary/Pods-MyPrivateLibrary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyPrivateLibrary : NSObject
+@end
+@implementation PodsDummy_Pods_MyPrivateLibrary
+@end

c52fc676d33f8c928fe9c465ea941ac7e4e03caf
Tamay Aykut <tamay.aykut@outlook.com>
2021-04-06 18:07:46 -0700

    add custom print

--- MyPrivateLibrary/MyPrivateLibrary/File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  MyPrivateLibrary
-//
-//  Created by Tamay Aykut on 4/6/21.
-//
-
-import Foundation
-import TensorFlowLite
---
+++ MyPrivateLibrary/MyPrivateLibrary/TFLiteLogger.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  MyPrivateLibrary
+//
+//  Created by Tamay Aykut on 4/6/21.
+//
+
+import Foundation
+//import TensorFlowLite
+
+public class TFLiteLogger {
+    public init() {
+        debugPrint("Tf lite initialized")
+    }
+}
